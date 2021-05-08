176d8d8db9e05465b1d86b8aad21439c89828ddd
David Ikin <shadow2272@aol.com>
2021-05-08 15:47:30 +0100

    PEP8 style & commentary

--- feetToCM.py
+++ feetToCM.py
@@ -1,8 +1,8 @@
-def cm(feet=0, inches=0):
+def cm(name, feet=0, inches=0): # keyword arguments (x=0) are positioned after non kwargs.
     '''Convert imperial measure to metric(feet & inches --> centimeter).'''
     inches_to_cm = inches * 2.54
     feet_to_cm = feet * 12 * 2.54
-    return inches_to_cm + feet_to_cm
+    return name, inches_to_cm + feet_to_cm
 
 
-print(cm(6, 0))
+print(cm('Dave', 6, 0))
