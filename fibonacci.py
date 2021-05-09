e49bfa9d14771925700745ede5333e54065bf025
David Ikin <shadow2272@aol.com>
2021-05-09 01:17:31 +0100

    Bug fixed

--- fibonacci.py
+++ fibonacci.py
@@ -2,12 +2,10 @@
 
 def fibonacci(n): # define the function and name it so you remember what it does 5 years from now
     '''Fibonacci sequence up to the n'th number''' # get used to writing docstrings
-    a = 0 # set up the start of the logic
-    b = 1 # as above ^
-    while a < n: # run code below until this statement becomes invalid which occurs when a becomes more than n
-        a = b # this is a counter, the loop stops when the condition is met
-        b = a + b # fibonacci's logic
-        print(a, end=' ') # end: print() method- value defaults to newline, ' ' keeps output on one line
-    print() # this resets the value passed to the 'end' method back to \n (newline), terminal says 'fanks'
-
-fibonacci(3000) # type the name of the function and the argument in the () lets do it! (enthusiasm)
+    a, b = 0, 1 # set up the logic for first 2 values in the fibonacci sequence
+    while a < n: # implements a counter in to the logic
+        print(a, end=' ') # uses one line in the terminal rather than multiple
+        a, b = b, a+b # fibonaccis logic, with embedded counter for the while loop
+    print() # resets the new line 
+    
+fibonacci(3000) # run fibonacci function up to the n'th number which we have decide is 3000
